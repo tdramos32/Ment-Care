27a861ea91b957d9912b6e5707c9443aa9932574
Tyler R <tdramos32@gmail.com>
2023-04-25 13:07:58 -0400

    changing models and view of report system

--- record_system/models.py
+++ record_system/models.py
@@ -1,43 +1,27 @@
 from django.db import models
 from django import forms
-
+from doctor.models import Prescription, Doctor_Information, Appointment
+from hospital.models import Patient
 # Create your models here.
+class diagnoses_info(models.Model):
+    diagnoses = models.CharField(max_length=64, null=True, blank=True)
+    date_of_diagnosis = models.DateField(auto_now_add=True, null=True)
+
 class record(models.Model):
-    patient_id = models.AutoField(primary_key=True)
-    doctor_id = models.IntegerField(null=True, blank=True)
-    doctor_name = models.CharField(max_length=100, null=True, blank=True) 
-    patient_name = models.CharField(max_length=100, null=True, blank=True)
-    patient_dob = models.DateField(auto_now_add=True, null=True)
-    patient_age = models.IntegerField(null=True, blank=True)
-    patient_diagnoses = models.TextField(max_length=1500, null=True, blank=True)
-    patient_drugs = models.TextField(max_length=1500, null=True, blank=True)
-    is_violent = models.BooleanField(default=False)
-    is_suicidal = models.BooleanField(default=False)
-    has_been_sectioned = models.BooleanField(default=False)
-    is_sectioned = models.BooleanField(default=False)
-    is_violent = models.BooleanField(default=False)
-
-    relationship_choices = (
-        ('1', 'Single'),
-        ('2', 'Married'),
-        ('3', 'Engaged'),
-        ('4', 'In a Relationship'),
-        ('5', "It's Complicated"),
-        ('6', 'Prefer not to say'),
-    )
-    relationship_status = forms.MultipleChoiceField(choices=relationship_choices)
-
-    suffix_choices = (
-        ('1','Mr.'),
-        ('2','Mrs.'),
-        ('3','Miss'),
-        ('4','Dr.'),
-        ('5','None'),
-    )
-    suffix = forms.MultipleChoiceField(choices=suffix_choices)
-    children_count = models.IntegerField(default=0,null=True,blank=True)
-    patient_history = models.TextField(null=True, blank=True)
-
+    record_id = models.AutoField(primary_key=True)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE, null=True, blank=True)
+    doctor = models.ForeignKey(Doctor_Information, on_delete=models.CASCADE, null=True, blank=True)
     def __str__(self) -> str:
         return(self.patient_name + "'s Mental Health Report") 
 
+#test
+class appointment_notes(models.Model):
+    appointment_notes_id = models.AutoField(primary_key=True)
+    appointment = models.ForeignKey(Appointment, on_delete=models.CASCADE, null=True, blank=True)
+    doctor = models.ForeignKey(Doctor_Information, on_delete=models.CASCADE, null=True, blank=True)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE, null=True, blank=True)
+    prescriptions = models.ForeignKey(Prescription, on_delete=models.CASCADE, null=True, blank=True)
+    #Will also need the medical staff which simon is making
+    notes = models.TextField(max_length=1000, null=True, blank=True)
+    next_appointment = models.DateField(auto_now_add=True, null=True)
+    diagnoses = models.ForeignKey(diagnoses_info, on_delete=models.CASCADE, null=True, blank=True)
