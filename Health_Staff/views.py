2ab3e0684c96a195d4717195d8cad9844100854e
simon-kurbiel <simonkurbiel08@gmail.com>
2023-04-26 00:29:00 -0400

    Added some views such as dashboards ...

--- Health_Staff/views.py
+++ Health_Staff/views.py
@@ -1,3 +1,77 @@
-from django.shortcuts import render
+import email
+from email.mime import image
+from multiprocessing import context
+from unicodedata import name
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import cache_control
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
+from hospital.models import Hospital_Information, User, Patient
+from django.db.models import Q
+from pharmacy.models import Medicine, Pharmacist
+from doctor.models import Doctor_Information, Prescription, Prescription_test, Report, Appointment, Experience , Education,Specimen,Test
+from pharmacy.models import Order, Cart
+from sslcommerz.models import Payment
+
+
+from hospital_admin.models import Admin_Information,specialization,service,hospital_department, Clinical_Laboratory_Technician, Test_Information
+import random,re
+import string
+from django.db.models import  Count
+from datetime import datetime
+import datetime
+from django.views.decorators.csrf import csrf_exempt
+
+from django.core.mail import BadHeaderError, send_mail
+from django.template.loader import render_to_string
+from django.http import HttpResponse
+from django.utils.html import strip_tags
+
 
 # Create your views here.
+def staff_dashboard(request):
+    return render(request,'Health_Staff/staff-dashboard')
+
+@csrf_exempt
+@login_required(login_url='admin_login')
+def labworker_dashboard(request):
+    if request.user.is_authenticated:
+        if request.user.is_labworker:
+            
+            lab_workers = Clinical_Laboratory_Technician.objects.get(user=request.user)
+            doctor = Doctor_Information.objects.all()
+            context = {'doctor': doctor,'lab_workers':lab_workers}
+            return render(request, 'Health_Staff/staff-dashboard.html',context)
+        
+@csrf_exempt
+@login_required(login_url='admin-login')
+def mypatient_list(request):
+    if request.user.is_authenticated:
+        if request.user.is_labworker:
+            lab_workers = Clinical_Laboratory_Technician.objects.get(user=request.user)
+            #report= Report.objects.all()
+            patient = Patient.objects.all()
+            context = {'patient': patient,'lab_workers':lab_workers}
+            return render(request, 'Health_Staff/mypatient-list.html',context)
+        
+
+def test_list(request):
+    if request.user.is_labworker:
+        lab_workers = Clinical_Laboratory_Technician.objects.get(user=request.user)
+        test = Test_Information.objects.all()
+        context = {'test':test,'lab_workers':lab_workers}
+    return render(request, 'Health_Staff/test-list.html',context)
+
+@csrf_exempt
+@login_required(login_url='admin-login')
+def prescription_list(request,pk):
+    if request.user.is_authenticated:
+        if request.user.is_labworker:
+            lab_workers = Clinical_Laboratory_Technician.objects.get(user=request.user)
+            patient = Patient.objects.get(patient_id=pk)
+            prescription = Prescription.objects.filter(patient=patient)
+            context = {'prescription': prescription,'lab_workers':lab_workers,'patient':patient}
+            return render(request, 'Health_Staff/prescription-list.html',context)
