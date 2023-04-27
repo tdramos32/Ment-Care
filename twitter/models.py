3ba510133e6bfc57f85e2a68f8ddec46639ad12e
lorenzolopez99 <lorlop99@gmail.com>
2023-04-27 04:12:31 -0400

    new tweet scraping code

--- twitter/models.py
+++ twitter/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Tweet(models.Model):
+    user = models.CharField(max_length=100)
+    rating = models.CharField(max_length=100)
+    text = models.CharField(max_length=100)
+    date = models.DateField(auto_now_add=True, null=True)
+    
+
+
+    def __str__(self):
+        return f'{self.user} : {self.rating} : {self.text} : {self.date}'
--- twitter/views.py
+++ twitter/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from django.shortcuts import redirect
+from twitter.models import Tweet
+from datetime import date
+import requests
+from bs4 import BeautifulSoup
 
 # Create your views here.
+
+
+
+
+
+
+
+
+def tweet(request):
+    user = request.user
+    acc = user.twitter
+
+    if request.method == 'POST':
+        num = request.POST['tweet']
+        
+        data = request.POST['tweet']
+        #label,score = trained_models.bertweet(data)
+        URL = "https://nitter.net/"+acc+"/with_replies"
+        page = requests.get(URL)
+
+        soup = BeautifulSoup(page.content, "html.parser")
+        results = soup.findall("div", class_="tweet-content media-body")
+
+        for r in results:
+            if len(r.contents):
+                if r.string is not None:
+                    tweet = r.string
+                    rating = "SEVERE"
+                    
+        
+        tweetinfo = Tweet(user = user, rating = rating, text = tweet)
+        tweetinfo.save()
+        
+        return redirect(reverse('patient-dashboard'))
+    
+    return render(request,'tweet/tweet.html',{'items':'none'})
