06fc54ed58a332e393329b35bdbe03c77e914f25
simon-kurbiel <simonkurbiel08@gmail.com>
2023-04-28 12:07:18 -0400

    took away some wrte privildges from the patient

--- doctor/views.py
+++ doctor/views.py
@@ -189,7 +189,7 @@ def doctor_dashboard(request):
 @login_required(login_url="doctor-login")
 def appointments(request):
     doctor = Doctor_Information.objects.get(user=request.user)
-    appointments = Appointment.objects.filter(doctor=doctor).filter(appointment_status='pending').order_by('date')
+    appointments = Appointment.objects.filter(doctor=doctor).filter(appointment_status='confirmed').order_by('date')
     context = {'doctor': doctor, 'appointments': appointments}
     return render(request, 'appointments.html', context) 
  
@@ -230,10 +230,7 @@ def accept_appointment(request, pk):
     html_message = render_to_string('appointment_accept_mail.html', {'values': values})
     plain_message = strip_tags(html_message)
     
-    try:
-        send_mail(subject, plain_message, 'hospital_admin@gmail.com',  [patient_email], html_message=html_message, fail_silently=False)
-    except BadHeaderError:
-        return HttpResponse('Invalid header found')
+    
     
     messages.success(request, 'Appointment Accepted')
     
@@ -262,12 +259,6 @@ def reject_appointment(request, pk):
     
     html_message = render_to_string('appointment_reject_mail.html', {'values': values})
     plain_message = strip_tags(html_message)
-    
-    try:
-        send_mail(subject, plain_message, 'hospital_admin@gmail.com',  [patient_email], html_message=html_message, fail_silently=False)
-    except BadHeaderError:
-        return HttpResponse('Invalid header found')
-    
     messages.error(request, 'Appointment Rejected')
     
     return redirect('doctor-dashboard')
@@ -451,11 +442,6 @@ def booking(request, pk):
             
             html_message = render_to_string('appointment-request-mail.html', {'values': values})
             plain_message = strip_tags(html_message)
-            
-            try:
-                send_mail(subject, plain_message, 'hospital_admin@gmail.com',  [patient_email], html_message=html_message, fail_silently=False)
-            except BadHeaderError:
-                return HttpResponse('Invalid header found')
         
         
         messages.success(request, 'Appointment Booked')
@@ -482,6 +468,7 @@ def my_patients(request):
 # def patient_profile(request):
 #     return render(request, 'patient_profile.html')
 
+
 @csrf_exempt
 @login_required(login_url="doctor-login")
 def patient_profile(request, pk):
@@ -492,7 +479,8 @@ def patient_profile(request, pk):
         appointments = Appointment.objects.filter(doctor=doctor).filter(patient=patient)
         prescription = Prescription.objects.filter(doctor=doctor).filter(patient=patient)
         report = Report.objects.filter(doctor=doctor).filter(patient=patient) 
-        mood = Mood.objects.filter(user = 'patient2').all()
+        mood = Mood.objects.filter(user = patient.username).all()
+        print(patient.username)
     else:
         redirect('doctor-logout')
     context = {'doctor': doctor, 'appointments': appointments, 'patient': patient, 'prescription': prescription, 'report': report, 'mood':mood}  
@@ -701,6 +689,7 @@ def doctor_review(request, pk):
         return render(request, 'doctor-profile.html', context)
     else:
         logout(request)
+        
  
  
    
