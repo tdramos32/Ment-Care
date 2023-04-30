196d20953f92c6f98710c0950b4b2e1363412a88
simon-kurbiel <simonkurbiel08@gmail.com>
2023-04-30 01:37:05 -0400

    changes

--- hospital_admin/signals.py
+++ hospital_admin/signals.py
@@ -1,7 +1,4 @@
-from django.db.models.signals import post_save, post_delete
-from django.dispatch import receiver 
-from hospital.models import User
-from hospital_admin.models import Clinical_Laboratory_Technician
+
 
 # Update and delete signals
 # @receiver(post_save, sender=Admin_Information)
