e010758c2f2d4ddc657a1cbc12c9102baa8ea274
simon-kurbiel <simonkurbiel08@gmail.com>
2023-04-30 21:31:04 -0400

    removed for deploying purposes

--- mood/views.py
+++ mood/views.py
@@ -13,17 +13,6 @@ def mood(request):
     user = request.user
    
    
-    if request.method == 'POST':
-        num = request.POST['mood']
-        
-
-
-        data = request.POST['mood']
-        label,score = trained_models.bertweet(data)
-
-        moodinfo = Mood(user = user, input = data, label = label)
-        moodinfo.save()
-        
-        return redirect(reverse('patient-dashboard'))
+    
     
     return render(request,'mood/mood.html',{'items':'none'})
