04500231e7beaddc3cda892e26ce4fc007eb69fa
simon-kurbiel <simonkurbiel08@gmail.com>
2023-05-03 02:54:03 -0400

    added some more functionalityies

--- mood/views.py
+++ mood/views.py
@@ -30,18 +30,19 @@ def mood(request):
 
         moodinfo = Mood(user = user, input = data, label = label)
         moodinfo.save()
-        twitter_user = str(request.POST['twitter'])
-        if(twitter_user == ""):
-            print("Didn't enter a username")
-        else:
-            tweetinfo = Twitter(user = user, tweeter_username = twitter_user, registered = "ON")
-            tweetinfo.save()
-            all_tweets = tweet_scraper(twitter_user)
-            for tweet in all_tweets:
-                output = clean_output(tweet)
-                the_tweet = Tweets(user = user, tweet = tweet, toxic = output['toxic'], severe_toxic = output['severe_toxic'], obscene = output['obscene'], threat = output['threat'] , insult = output['insult'], identity_hate = output['identity_hate'])
-                print(the_tweet.toxic)
-                the_tweet.save()
+        if has_user_name == False:
+            twitter_user = str(request.POST['twitter'])
+            if(twitter_user == ""):
+                print("Didn't enter a username")
+            else:
+                tweetinfo = Twitter(user = user, tweeter_username = twitter_user, registered = "ON")
+                tweetinfo.save()
+                all_tweets = tweet_scraper(twitter_user)
+                for tweet in all_tweets:
+                    output = clean_output(tweet)
+                    the_tweet = Tweets(user = user, tweet = tweet, toxic = output['toxic'], severe_toxic = output['severe_toxic'], obscene = output['obscene'], threat = output['threat'] , insult = output['insult'], identity_hate = output['identity_hate'])
+                    print(the_tweet.toxic)
+                    the_tweet.save()
             
         return redirect(reverse('patient-dashboard'))
     
