68de061333950e6c71de4124cccb7f5c15499e67
simon-kurbiel <simonkurbiel08@gmail.com>
2023-05-03 01:17:27 -0400

    added Twitter and Tweets model

--- mood/models.py
+++ mood/models.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 # Create your models here.
 class Mood(models.Model):
+
     user = models.CharField(max_length=100)
     input = models.CharField(max_length=100)
     label = models.CharField(max_length=100)
@@ -13,3 +14,22 @@ class Mood(models.Model):
 
     def __str__(self):
         return f'{self.user} : {self.input} : {self.label} : {self.date}'
+
+class Twitter(models.Model):
+
+    user = models.CharField(max_length=100)
+    tweeter_username = models.CharField(max_length=200, null=True, blank=True)
+    registered = models.CharField(max_length=200, null=True, blank=True, default = 'OFF')
+
+    def __str__(self):
+        return f'{self.user} : {self.tweeter_username} : {self.registered}'
+
+class Tweets(models.Model):
+    user = models.CharField(max_length=100)
+    tweet = models.CharField(max_length = 100)
+    toxic = models.CharField(max_length = 6)
+    severe_toxic = models.CharField(max_length = 6)
+    obscene = models.CharField(max_length = 6)
+    threat = models.CharField(max_length = 6)
+    insult = models.CharField(max_length = 6)
+    identity_hate = models.CharField(max_length = 6)
