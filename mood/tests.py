cfa3d092aa3ab24a814b4a7c109295fdbccb9c7c
simon-kurbiel <simonkurbiel08@gmail.com>
2023-05-03 01:18:47 -0400

    added some tests

--- mood/tests.py
+++ mood/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
 # Create your tests here.
+from train import *
+from scraper import *
+def getSent(data):
+    
+   
+    output = clean_output(data)
+    toxic = output['toxic']
+    severe_toxic = output['severe_toxic']
+    obscene = output['obscene']
+    threat = output['threat']
+    insult = output['insult']
+    identity_hate = output['identity_hate']
+
+tweets = tweet_scraper('elonmusk')
+
+for tweet in tweets:
+    print(tweet,clean_output(tweet))
